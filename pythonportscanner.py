414b695aa349ce5c89e4e951c95561999c092707
alperenkesk <alperenkesk55@gmail.com>
2017-04-02 16:57:28 +0300

    Update pythonportscanner.py

--- pythonportscanner.py
+++ pythonportscanner.py
@@ -11,12 +11,9 @@ try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
-            print "Port:", port , "\t" "Open".format(port)
+            print "Port:", port , "\t" "Open"
         sock.close()
 
 except KeyboardInterrupt:
     print "Hoscakal..."
     sys.exit()
-
-
-
